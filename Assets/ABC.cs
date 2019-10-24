c021828a0609c2b3644ed2023b36b98c994f4979
raugh135 <55518299+raugh135@users.noreply.github.com>
2019-10-24 10:23:44 +0800

    Initial commit

---
+++ Assets/ABC.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ABC : MonoBehaviour {
+    private Animator ani;
+
+    // Use this for initialization
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ani.SetTrigger("Dancing");
+        }
+
+        ani.SetBool("Run", Input.GetKey(KeyCode.Space));
+    }
+}
